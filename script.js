1d70eef011c19326e4d5758f867e99a054834958
AwesomeWhiteWolf <148298503+AwesomeWhiteWolf@users.noreply.github.com>
2024-03-05 11:34:37 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -16,18 +16,21 @@ let flagBooks = false;
 let flagEvents = false;
 const word = document.getElementById('word');
 
-const characters = ["Авраам", "Иосиф", "Моисей"];
+const characters = ["Адам", "Ева", "Ной", "Авраам", "Сарра", "Иаков", "Моисей", "Давид", "Мария и Иосиф", "Иоанн Креститель", "Петр (Симон)", "Лазарь", "Иосиф", "Соломон", "Илия", "Иисус Христос",  
+"Каин и Авель", "Есфирь", "Раав", "Руфь", "Самуил", "Даниил", "Лазарь", "Никодим", 
+"Аполлос", "Гедеон", "Аггей", "Захария", "Амос", "Наум"];
+
 const books = ["Бытие", "Исход", "Псалмы", "Притчи", "Екклесиаст", "От Матфея", "Иона", "От Марка", "От Луки", "От Иоанна", "Деяния", "Римлянам", "1 Коринфянам", "2 Коринфянам", "Откровение", 
 "Левит", "Числа", "Второзаконие", "Иисус Навин", "Руфь", "1 Царств", "2 Царств", "3 Царств", "4 Царств", "Есфирь", "Иов", "Песня песней", "Исаия", "Иеремия", "Даниил", "Иакова", "Иуда", "Филиппийцам", "1 Тимофею", "2 Тимофею", "Титу", "Евреям", "Судьи", 
 "1 Паралипоменон", "2 Паралипоменон", "Ездра", "Неемия", "Плач Иеремии", "Иезекииль", "Осия", "Иоиль", "Амос", "Авдий", "Михей", "Наум", "Аввакум", "Софония", "Аггей", "Захария", "Малахия", "1 Петра", "2 Петра", "1 Иоанна", "2 Иоанна", "3 Иоанна", "Галатам", "Ефесянам", "1 Фессалоникийцам", "2 Фессалоникийцам", "Филимону"];
-const events = ["Великий потоп", "10 казней египетских", "Давид против Галиафа"];
+
+const events = ["Пропавшая овца", "Всемирный потоп", "Содом и Гоморра", "Давид Против Голиафа", "Добрый Самарянин", "Вавилонская башня", "Волхвы и звезда", "Крещение Иисуса", "Пост Иисуса в пустыне", 
+"Нагорная проповедь", "Хождение по воде", "Блудный сын", "Фарисей и мытарь", "Богатый юноша", 
+"Павел и Сила в темнице", "Моисей и медный змей", "Раав и соглядатаи"];
+
 let finalArray = [];
 
 var score = 0;
-function wordChoice() {
-  let randNum = Math.floor(Math.random() * finalArray.length);
-  word.textContent = finalArray[randNum];
-}
 
 function startGame() {
   let timer = time.value;
@@ -37,26 +40,50 @@ function startGame() {
   } else { 
     timerText.textContent = "Время: " + time.value;
   }
-  setInterval(function() {
-    timerText.textContent = "Время: " + timer;
+  const intervalId = setInterval(function() {
     timer--;
+    timerText.textContent = "Время: " + timer;
     if (timer < 0) {
       alert("Вcе");
+      screenFirst();
+      clearInterval(intervalId);
     }
   }, 1000);
   if (flagChar == true) {
-    finalArray.push(...characters);
+    if (complexity.value == "Простая" || complexity.value == "") {
+      finalArray.push(...(characters.slice(0, 16)));
+    }
+    else if (complexity.value == "Средняя") {
+      finalArray.push(...(characters.slice(16, 24)));
+    }
+    else if (complexity.value == "Сложная") {
+      finalArray.push(...(characters.slice(24, characters.length)));
+    }
   }
   if (flagBooks == true) {
-    finalArray.push(...books);
+    if (complexity.value == "Простая" || complexity.value == "") {
+      finalArray.push(...(books.slice(0, 15)));
+    }
+    else if (complexity.value == "Средняя") {
+      finalArray.push(...(books.slice(15, 38)));
+    }
+    else if (complexity.value == "Сложная") {
+      finalArray.push(...(books.slice(38, books.length)));
+    }
   }
   if (flagEvents == true) {
-    finalArray.push(...events);
+    if (complexity.value == "Простая" || complexity.value == "") {
+      finalArray.push(...(events.slice(0, 9)));
+    }
+    else if (complexity.value == "Средняя") {
+      finalArray.push(...(events.slice(9, 14)));
+    }
+    else if (complexity.value == "Сложная") {
+      finalArray.push(...(events.slice(14, events.length)));
+    }
   }
   wordChoice();
-  document.querySelector("h1").style.visibility = "hidden";
-  forHide.style.display = "none";
-  forHide1.style.display = "block";
+  screenSecond();
 }
 
 function checkCheck() {
@@ -82,6 +109,27 @@ function checkCheck() {
     flagEvents = false;
   }
 }
+
+function wordChoice() {
+  let randNum = Math.floor(Math.random() * finalArray.length);
+  word.textContent = finalArray[randNum];
+}
+
+function screenFirst() {
+  document.querySelector("h1").style.visibility = "visible";
+  forHide.style.display = "block";
+  forHide1.style.display = "none";
+  complexity.value = "";
+}
+
+function screenSecond() {
+  document.querySelector("h1").style.visibility = "hidden";
+  forHide.style.display = "none";
+  forHide1.style.display = "block";
+}
+
+
+
 
 let startingX, startingY, movingX, movingY;
 function touchStart(e){
