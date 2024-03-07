9b34edcab46611feac36e66b02504c6543dd0359
AwesomeWhiteWolf <148298503+AwesomeWhiteWolf@users.noreply.github.com>
2024-03-07 19:10:07 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -16,70 +16,89 @@ let flagBooks = false;
 let flagEvents = false;
 const word = document.getElementById('word');
 
-const characters = ["Адам", "Ева", "Ной", "Авраам", "Сарра", "Иаков", "Моисей", "Давид", "Мария и Иосиф", "Иоанн Креститель", "Петр (Симон)", "Лазарь", "Иосиф", "Соломон", "Илия", "Иисус Христос",  
-"Каин и Авель", "Есфирь", "Раав", "Руфь", "Самуил", "Даниил", "Лазарь", "Никодим", 
-"Аполлос", "Гедеон", "Аггей", "Захария", "Амос", "Наум"];
+const characters = [
+  ["Адам", "Ева", "Ной", "Авраам", "Сарра", "Иаков", "Моисей", "Давид", "Мария и Иосиф", 
+  "Иоанн Креститель", "Петр (Симон)", "Лазарь", "Иосиф", "Соломон", "Илия", "Иисус Христос"], //e
+  ["Каин и Авель", "Есфирь", "Раав", "Руфь", "Самуил", "Даниил", "Лазарь", "Никодим"], //n
+  ["Аполлос", "Гедеон", "Аггей", "Захария", "Амос", "Наум"] //h
+];
 
-const books = ["Бытие", "Исход", "Псалмы", "Притчи", "Екклесиаст", "От Матфея", "Иона", "От Марка", "От Луки", "От Иоанна", "Деяния", "Римлянам", "1 Коринфянам", "2 Коринфянам", "Откровение", 
-"Левит", "Числа", "Второзаконие", "Иисус Навин", "Руфь", "1 Царств", "2 Царств", "3 Царств", "4 Царств", "Есфирь", "Иов", "Песня песней", "Исаия", "Иеремия", "Даниил", "Иакова", "Иуда", "Филиппийцам", "1 Тимофею", "2 Тимофею", "Титу", "Евреям", "Судьи", 
-"1 Паралипоменон", "2 Паралипоменон", "Ездра", "Неемия", "Плач Иеремии", "Иезекииль", "Осия", "Иоиль", "Амос", "Авдий", "Михей", "Наум", "Аввакум", "Софония", "Аггей", "Захария", "Малахия", "1 Петра", "2 Петра", "1 Иоанна", "2 Иоанна", "3 Иоанна", "Галатам", "Ефесянам", "1 Фессалоникийцам", "2 Фессалоникийцам", "Филимону"];
+const books = [
+  ["Бытие", "Исход", "Псалмы", "Притчи", "Екклесиаст", "От Матфея", "Иона", "От Марка", "От Луки", "От Иоанна", "Деяния", "Римлянам", 
+  "1 Коринфянам", "2 Коринфянам", "Откровение"], //e
+  ["Левит", "Числа", "Второзаконие", "Иисус Навин", "Руфь", "1 Царств", "2 Царств", "3 Царств", "4 Царств", "Есфирь", "Иов", "Песня песней", 
+  "Исаия", "Иеремия", "Даниил", "Иакова", "Иуда", "Филиппийцам", "1 Тимофею", "2 Тимофею", "Титу", "Евреям", "Судьи"], //n
+  ["1 Паралипоменон", "2 Паралипоменон", "Ездра", "Неемия", "Плач Иеремии", "Иезекииль", "Осия", "Иоиль", "Амос", "Авдий", "Михей", "Наум", 
+  "Аввакум", "Софония", "Аггей", "Захария", "Малахия", "1 Петра", "2 Петра", "1 Иоанна", "2 Иоанна", "3 Иоанна", "Галатам", "Ефесянам", 
+  "1 Фессалоникийцам", "2 Фессалоникийцам", "Филимону"] //h
+];
 
-const events = ["Пропавшая овца", "Всемирный потоп", "Содом и Гоморра", "Давид Против Голиафа", "Добрый Самарянин", "Вавилонская башня", "Волхвы и звезда", "Крещение Иисуса", "Пост Иисуса в пустыне", 
-"Нагорная проповедь", "Хождение по воде", "Блудный сын", "Фарисей и мытарь", "Богатый юноша", 
-"Павел и Сила в темнице", "Моисей и медный змей", "Раав и соглядатаи"];
+const events = [
+  ["Пропавшая овца", "Всемирный потоп", "Содом и Гоморра", "Давид Против Голиафа", "Добрый Самарянин", "Вавилонская башня", 
+  "Волхвы и звезда", "Крещение Иисуса", "Пост Иисуса в пустыне"], //e
+  ["Нагорная проповедь", "Хождение по воде", "Блудный сын", "Фарисей и мытарь", "Богатый юноша"] , //n
+  ["Павел и Сила в темнице", "Моисей и медный змей", "Раав и соглядатаи"] //h
+];
 
 let finalArray = [];
 
 var score = 0;
+var timer = time.value;
 
 function startGame() {
-  let timer = time.value;
+  let flagEnd = false;
   if (time.value == "") {
     timerText.textContent = "Время: " + 60; 
     timer = 60;
   } else { 
     timerText.textContent = "Время: " + time.value;
+    timer = time.value;
   }
   const intervalId = setInterval(function() {
     timer--;
     timerText.textContent = "Время: " + timer;
+    if (timer == 1) {
+      flagEnd = true;
+    }
     if (timer < 0) {
-      alert("Вcе");
       screenFirst();
+      if (flagEnd === true) {
+        alert("Вcе");
+      }
       clearInterval(intervalId);
     }
   }, 1000);
   if (flagChar == true) {
     if (complexity.value == "Простая" || complexity.value == "") {
-      finalArray.push(...(characters.slice(0, 16)));
+      finalArray.push(...(characters[0]));
     }
     else if (complexity.value == "Средняя") {
-      finalArray.push(...(characters.slice(16, 24)));
+      finalArray.push(...(characters[1]));
     }
     else if (complexity.value == "Сложная") {
-      finalArray.push(...(characters.slice(24, characters.length)));
+      finalArray.push(...(characters[2]));
     }
   }
   if (flagBooks == true) {
     if (complexity.value == "Простая" || complexity.value == "") {
-      finalArray.push(...(books.slice(0, 15)));
+      finalArray.push(...(books[0]));
     }
     else if (complexity.value == "Средняя") {
-      finalArray.push(...(books.slice(15, 38)));
+      finalArray.push(...(books[1]));
     }
     else if (complexity.value == "Сложная") {
-      finalArray.push(...(books.slice(38, books.length)));
+      finalArray.push(...(books[2]));
     }
   }
   if (flagEvents == true) {
     if (complexity.value == "Простая" || complexity.value == "") {
-      finalArray.push(...(events.slice(0, 9)));
+      finalArray.push(...(events[0]));
     }
     else if (complexity.value == "Средняя") {
-      finalArray.push(...(events.slice(9, 14)));
+      finalArray.push(...(events[1]));
     }
     else if (complexity.value == "Сложная") {
-      finalArray.push(...(events.slice(14, events.length)));
+      finalArray.push(...(events[2]));
     }
   }
   wordChoice();
@@ -117,6 +136,9 @@ function wordChoice() {
     if ([...new Set(finalArray)].length == 1) {
       alert("Слова кончились, скоро добавим больше)");
       screenFirst();
+      score = 0;
+      scoreText.textContent = "Очки: 0";
+      timer = 0;
       break;
     }
   }
@@ -124,15 +146,24 @@ function wordChoice() {
   delete finalArray[randNum];
 }
 
+function backTo() {
+  screenFirst();
+  timer = 0;
+}
+
 function screenFirst() {
-  document.querySelector("h1").style.visibility = "visible";
+  document.querySelector("h1").style.display = "block";
+  document.querySelector("#backBtn").style.display = "none";
   forHide.style.display = "block";
   forHide1.style.display = "none";
   complexity.value = "";
+  score = 0;
+  scoreText.textContent = "Очки: 0";
 }
 
 function screenSecond() {
-  document.querySelector("h1").style.visibility = "hidden";
+  document.querySelector("h1").style.display = "none";
+  document.querySelector("#backBtn").style.display = "block";
   forHide.style.display = "none";
   forHide1.style.display = "block";
 }
@@ -152,9 +183,15 @@ function touchMove(e){
 function touchEnd() {
     if(startingX+50 < movingX) {
         score++;
+        playSound("facebook_sms.mp3");
     } else if(startingX-50 > movingX) {
         score--;
+        playSound("oshibka-v-kompyutere.mp3");
     }
     scoreText.textContent = "Очки: " + score;
     wordChoice();
+}
+function playSound(audioName) {
+  let audio = new Audio(audioName);
+  audio.play();
 }
